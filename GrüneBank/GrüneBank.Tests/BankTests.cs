c0715e5de0b048d6c8802df7d9b1fc298946f4d6
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-26 14:05:56 +0200

    1) Alle Tests für die Bank schreiben und die Methoden + Properties automatisch generieren lassen

--- GrüneBank/GrüneBank.Tests/BankTests.cs
+++ GrüneBank/GrüneBank.Tests/BankTests.cs
@@ -7,8 +7,81 @@ namespace GrüneBank.Tests
     public class BankTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Bank_can_create_instance()
         {
+            Bank b1 = new Bank();
+            Assert.IsNotNull(b1);
+            Assert.AreEqual(0, b1.Balance);
         }
+
+        [TestMethod]
+        public void Bank_constructor_with_valid_value_sets_balance()
+        {
+            Bank b1 = new Bank(10_000m);
+
+            Assert.AreEqual(10_000m, b1.Balance);
+        }
+
+        [TestMethod]
+        public void Bank_constructor_with_invalid_value_throws_ArgumentException()
+        {
+            Assert.ThrowsException<ArgumentException>(() =>
+           {
+               Bank b1 = new Bank(-10_000m);
+           });
+        }
+
+        [TestMethod]
+        [DataRow(2_000)]
+        [DataRow(20_000)]
+        [DataRow(200_000)]
+        public void Deposit_with_valid_value_sets_Balance(decimal depositValue)
+        {
+            Bank b1 = new Bank();
+
+            Assert.AreEqual(0, b1.Balance);
+            b1.Deposit(depositValue);
+            Assert.AreEqual(depositValue, b1.Balance);
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(-50)]
+        public void Deposit_with_invalid_value_throws_ArgumentException(decimal depositValue)
+        {
+            Bank b1 = new Bank();
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                b1.Deposit(depositValue);
+            });
+        }
+
+        [TestMethod]
+        [DataRow(10_000,2_000)]
+        [DataRow(20_000,20_000)] // Auf exakt 0 abheben
+        [DataRow(300_000,200_000)]
+        public void Withdraw_with_valid_value_sets_Balance(decimal startBalance,decimal withdrawValue)
+        {
+            Bank b1 = new Bank(startBalance);
+
+            b1.Withdraw(withdrawValue);
+            Assert.AreEqual(startBalance - withdrawValue, b1.Balance);
+        }
+
+        [TestMethod]
+        [DataRow(10_000, 100_000)]
+        [DataRow(20_000, 200_000)]
+        public void Withdraw_more_than_Balance_throws_ArgumentException(decimal startBalance, decimal withdrawValue)
+        {
+            Bank b1 = new Bank(startBalance);
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                b1.Withdraw(withdrawValue);
+            });
+        }
+
+
     }
 }
--- GrüneBank/GrüneBank/Bank.cs
+++ GrüneBank/GrüneBank/Bank.cs
@@ -8,5 +8,27 @@ namespace GrüneBank
 {
     public class Bank
     {
+        private decimal v;
+
+        public Bank()
+        {
+        }
+
+        public Bank(decimal v)
+        {
+            this.v = v;
+        }
+
+        public decimal Balance { get; set; }
+
+        public void Deposit(decimal v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Withdraw(decimal withdrawValue)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
