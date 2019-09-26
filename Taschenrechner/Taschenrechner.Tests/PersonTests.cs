06afc21bdd548cfe18b1304f4b1ca70f2c7a2c6e
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-26 12:04:05 +0200

    Übung: Person.Equals mit Tests

---
+++ Taschenrechner/Taschenrechner.Tests/PersonTests.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner.Tests
+{
+    [TestClass]
+    public class PersonTests
+    {
+        // testm +TAB+TAB
+        [TestMethod]
+        public void Equals_with_null_returns_false()
+        {
+            // Entsprechenden Unit-Tests für Person 
+
+            // Vorbereitung
+            Person p = new Person();
+            p.Vorname = "Max";
+            p.Nachname = "Müller";
+            p.Alter = 33;
+            p.Kontostand = 6000;
+
+            Assert.IsFalse(p.Equals(null)); // Erwartetes ergebnis: false
+        }
+        [TestMethod]
+        public void Equals_with_wrong_datatypes_returns_false()
+        {
+            // Entsprechenden Unit-Tests für Person 
+
+            // Vorbereitung
+            Person p = new Person();
+            p.Vorname = "Max";
+            p.Nachname = "Müller";
+            p.Alter = 33;
+            p.Kontostand = 6000;
+
+            int x = 0;
+
+            Assert.IsFalse(p.Equals(x)); // Erwartetes ergebnis: false
+        }
+        [TestMethod]
+        public void Equals_with_ref_not_equ_returns_false()
+        {
+            // Entsprechenden Unit-Tests für Person 
+
+            // Vorbereitung
+            Person p = new Person();
+            p.Vorname = "Max";
+            p.Nachname = "Müller";
+            p.Alter = 33;
+            p.Kontostand = 6000;
+
+            Person p2 = p;
+
+            Assert.IsTrue(p.Equals(p2)); // Erwartetes ergebnis: true
+        }
+        [TestMethod]
+        public void Equals_with_same_values_returns_true()
+        {
+            // Entsprechenden Unit-Tests für Person 
+
+            // Vorbereitung
+            Person p = new Person();
+            p.Vorname = "Max";
+            p.Nachname = "Müller";
+            p.Alter = 33;
+            p.Kontostand = 6000;
+
+            Person p2 = new Person();
+            p2.Vorname = "Max";
+            p2.Nachname = "Müller";
+            p2.Alter = 33;
+            p2.Kontostand = 6000;
+
+            Assert.IsTrue(p.Equals(p2)); // Erwartetes ergebnis: true
+        }
+        [TestMethod]
+        public void Equals_with_different_values_returns_false()
+        {
+            // Entsprechenden Unit-Tests für Person 
+
+            // Vorbereitung
+            Person p = new Person();
+            p.Vorname = "Max";
+            p.Nachname = "Müller";
+            p.Alter = 33;
+            p.Kontostand = 6000;
+
+            Person p2 = new Person();
+            p2.Vorname = "Anna";
+            p2.Nachname = "Nass";
+            p2.Alter = 33;
+            p2.Kontostand = 6000;
+
+            Assert.IsFalse(p.Equals(p2)); // Erwartetes ergebnis: false
+        }
+    }
+}
---
+++ Taschenrechner/Taschenrechner/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    public class Person
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public byte Alter { get; set; }
+        public decimal Kontostand { get; set; }
+
+        public override bool Equals(object obj)
+        {
+
+            // Wenn der Datentyp richtig ist:
+
+            //--------> Wenn nicht: sind die Werte gleich? -> True
+            //----------> Wenn nicht: false
+
+            // Seit C# 7.0:
+            // if(obj is Person == false) -> False
+
+            // Wenn der Parameter null ist -> False
+            if (obj == null)
+                return false;
+
+            // Wenn der Datentyp falsch ist -> false
+            if (obj.GetType() != typeof(Person))
+                return false;
+
+            // -----> Sind die Referenzen gleich ? 
+            if (this != obj)
+            {
+                // Referenzen ungleich ---> Werte gleich?
+                if (this.Vorname != ((Person)obj).Vorname ||
+                        this.Alter != ((Person)obj).Alter ||
+                        this.Nachname != ((Person)obj).Nachname ||
+                        this.Kontostand != ((Person)obj).Kontostand)
+                    return false;
+
+            }
+
+            return true;
+        }
+    }
+}
