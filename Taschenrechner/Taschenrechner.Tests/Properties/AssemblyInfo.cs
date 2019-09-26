4aeed410bd5386871726adeb7946c86167110f8e
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-26 10:03:27 +0200

    Erster UnitTest nach AAA-Prinzip aufgebaut

---
+++ Taschenrechner/Taschenrechner.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Taschenrechner.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Taschenrechner.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84174780-7a25-49b4-9798-1b4747101cb6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Taschenrechner/Taschenrechner.Tests/RechnerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Taschenrechner.Tests
+{
+    [TestClass]
+    public class RechnerTests
+    {
+        [TestMethod]
+        public void Add_2_and_3_returns_5()
+        {
+            // Arrange: Vorbereitung
+            Rechner r = new Rechner();
+
+            // Act: den zu testenden Code ausführen
+            var result = r.Add(2, 3);
+
+            // Assert: Ergebnis interpretieren
+            Assert.AreEqual(8, result);
+        }
+    }
+}
