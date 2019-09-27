85737f6e9a008a2f85a8996754cb67ae64b7dfbc
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-27 09:46:29 +0200

    Basiskonfiguration für das EF erstellt

---
+++ Personenverwaltung/ppedv.Personenverwaltung.Data.EF/EFContext.cs
@@ -0,0 +1,26 @@
+﻿using ppedv.Personeverwaltung.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.Personenverwaltung.Data.EF
+{
+   // Konfiguration für das EF
+    public class EFContext : DbContext
+    {
+        // Für TN-PCs:
+        // @"Server=.;Database=Personenverwaltung_Produktiv;Trusted_Connection=true"
+
+        // Für Trainer-PC (Michi):
+        // @"Server=(localdb)\MSSQLLocalDB;Database=Personenverwaltung_Produktiv;Trusted_Connection=true;AttachDbFilename=C:\temp\Personenverwaltung.mdf"
+        public EFContext() : this(@"Server=(localdb)\MSSQLLocalDB;Database=Personenverwaltung_Produktiv;Trusted_Connection=true;AttachDbFilename=C:\temp\Personenverwaltung.mdf") { }
+        public EFContext(string connectionString) : base(connectionString){}
+
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Abteilung> Abteilung { get; set; }
+        public DbSet<Firma> Firma { get; set; }
+    }
+}
