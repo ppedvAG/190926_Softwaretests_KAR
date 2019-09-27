95e5baf9833fc5ad100fc42af3f10307c54dc4e0
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-27 14:03:28 +0200

    Interface für das Datenbankrepository erstellt

---
+++ Personenverwaltung/ppedv.Personeverwaltung.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.Personeverwaltung.Domain.Interfaces
+{
+    public interface IRepository
+    {
+        void Add<T>(T item) where T : Entity;
+        void Delete<T>(T item) where T : Entity;
+        void Update<T>(T item) where T : Entity;
+        T GetByID<T>(int id) where T : Entity;
+        IEnumerable<T> GetAll<T>() where T : Entity;
+    }
+}
