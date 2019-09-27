f00d9a2b5e068579e6ff238c97e3b0dffb81e13f
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-27 11:03:21 +0200

    Fluentassertions und Autofixture

--- Personenverwaltung/ppedv.Personenverwaltung.Data.EF.Tests/EFContextTests.cs
+++ Personenverwaltung/ppedv.Personenverwaltung.Data.EF.Tests/EFContextTests.cs
@@ -1,4 +1,7 @@
 ﻿ using System;
+using System.Linq;
+using AutoFixture;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ppedv.Personeverwaltung.Domain;
 
@@ -68,14 +71,129 @@ namespace ppedv.Personenverwaltung.Data.EF.Tests
                 context.Person.Remove(loadedPerson);
                 context.SaveChanges();
             }
-
+             
 
             // Check Delete
             using (EFContext context = new EFContext(connectionString))
             {
                 Person loadedPerson = context.Person.Find(p1.ID);
                 Assert.IsNull(loadedPerson);
+             }
+        }
+
+        [TestMethod]
+        public void EFContext_can_CRUD_Person_FluentAssertions()
+        {
+            Person p1 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+            string newVorname = "Peter";
+
+            // Create
+            using (EFContext context = new EFContext(connectionString))
+            {
+                context.Person.Add(p1); // SQL-Insert
+                context.SaveChanges();
+            }
+
+            // Check Create
+            using (EFContext context = new EFContext(connectionString))
+            {
+                Person loadedPerson = context.Person.Find(p1.ID);
+                // Check: Read
+                // Assert.IsTrue(loadedPerson.Vorname == p1.Vorname); // ToDo: Richtig: ObjectGraph
+
+                //loadedPerson.Vorname.Should().Be(p1.Vorname);
+
+                //ObjectGraph:
+                loadedPerson.Should().BeEquivalentTo(p1); // Vergleicht alle Properties
+
+                // Update
+                loadedPerson.Vorname = newVorname; //EF: Changetracker
+                context.SaveChanges();
+            }
+
+            // Check Update
+            using (EFContext context = new EFContext(connectionString))
+            {
+                Person loadedPerson = context.Person.Find(p1.ID);
+                // Check: Read
+                loadedPerson.Vorname.Should().Be(newVorname);
+
+
+                // Delete
+                context.Person.Remove(loadedPerson);
+                context.SaveChanges();
+            }
+
+            // Check Delete
+            using (EFContext context = new EFContext(connectionString))
+            {
+                Person loadedPerson = context.Person.Find(p1.ID);
+                loadedPerson.Should().BeNull();
             }
         }
+
+
+        // https://github.com/AutoFixture/AutoFixture/wiki/Cheat-Sheet
+        [TestMethod]
+        public void EFContext_Autofixture_Test()
+        {
+            Fixture fix = new Fixture(); // Fixture-Objekt
+            var zufälligePerson = fix.CreateMany<Person>(1000).ToArray();
+
+            var top100Firmen = fix.CreateMany<Firma>(100).ToArray();
+        }
+
+        [TestMethod]
+        public void EFContext_can_CRUD_Person_FluentAssertions_Autofixture()
+        {
+            Fixture fix = new Fixture();
+            Person p1 = fix.Create<Person>();
+            string newVorname = fix.Create<string>();
+
+            // Create
+            using (EFContext context = new EFContext(connectionString))
+            {
+                context.Person.Add(p1); // SQL-Insert
+                context.SaveChanges();
+            }
+
+            // Check Create
+            using (EFContext context = new EFContext(connectionString))
+            {
+                Person loadedPerson = context.Person.Find(p1.ID);
+                // Check: Read
+                // Assert.IsTrue(loadedPerson.Vorname == p1.Vorname); // ToDo: Richtig: ObjectGraph
+
+                //loadedPerson.Vorname.Should().Be(p1.Vorname);
+
+                //ObjectGraph:
+                loadedPerson.Should().BeEquivalentTo(p1); // Vergleicht alle Properties
+
+                // Update
+                loadedPerson.Vorname = newVorname; //EF: Changetracker
+                context.SaveChanges();
+            }
+
+            // Check Update
+            using (EFContext context = new EFContext(connectionString))
+            {
+                Person loadedPerson = context.Person.Find(p1.ID);
+                // Check: Read
+                loadedPerson.Vorname.Should().Be(newVorname);
+
+
+                // Delete
+                context.Person.Remove(loadedPerson);
+                context.SaveChanges();
+            }
+
+            // Check Delete
+            using (EFContext context = new EFContext(connectionString))
+            {
+                Person loadedPerson = context.Person.Find(p1.ID);
+                loadedPerson.Should().BeNull();
+            }
+        }
+
     }
 }
